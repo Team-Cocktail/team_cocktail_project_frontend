e8853cdaab58aaa4647522bd85659eabb493d7c2
David Scattergood <davescattz@hotmail.com>
2019-12-15 14:12:52 +0000

    new commit with files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './images/tippl_no_backbround_logo.png';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+/*import logo from './logo.svg';*/
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          **PROJECT COCKTAIL**
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
